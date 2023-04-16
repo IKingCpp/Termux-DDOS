e83e040e5e0a4139a739329dd52004bf87dc6811
IKingCpp <104319800+IKingCpp@users.noreply.github.com>
2023-04-16 20:51:28 +0800

    Update DDOS.py

--- DDOS.py
+++ DDOS.py
@@ -1,27 +1,26 @@
+import time
+
 print("\033[01;31mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM")
-
 print("\033[01;31mM       YMM M       YMM MMP     YMM MP       MM")
-
 print("\033[01;31mM  mmmm   M M  mmmm   M M   mmm   M M  mmmmm  M")
-
 print("\033[01;31mM  MMMMM  M M  MMMMM  M M  MMMMM  M M        YM")
-
 print("\033[01;31mM  MMMMM  M M  MMMMM  M M  MMMMM  M MMMMMMM   M")
-
 print("\033[01;31mM  MMMM   M M  MMMM   M M   MMM   M M   MMM   M")
-
 print("\033[01;31mM        MM M        MM MMb     dMM Mb       dM")
-
 print("\033[01;31mMMMMMMMMMMM MMMMMMMMMMM MMMMMMMMMMM MMMMMMMMMMM")
 
+
 print("\033[01;33mAuthor:     \033[01;37mIKingCpp")
+print("\033[01;33mSource:     \033[01;37mNot Published")
 
-print("\033[01;33mSource:     \033[01;37mhttps://github.com/IKingCpp/Termux-DDOS")
 
 print("\033[01;37m------------")
-
 print("\033[01;37mINPUT TARGET")
-
 print("\033[01;37m------------")
 
-ddos = input("[*]\033[01;31mIP OR HOSTNAME :")
+host = input("[*]\033[01;31mIP OR HOSTNAME :\033[01;37m ")
+port = input("[*]\033[01;31mPORT : \033[01;37m ")
+
+print("[*]\033[01;31mAttacking : \033[01;37m", host, " ")
+time.sleep(2)
+print("[*]\033[01;31mERROR")
